14136ac9b54085b8203cd78211b0225f4e588cb7
shibin-dev <shibinflutter@gmail.com>
2022-03-07 17:41:12 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Testing App'),
     );
